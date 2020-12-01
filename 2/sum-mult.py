8268682c3b3089030c01371333a75b895b810514
Andrew Rueckert <ruke47@users.noreply.github.com>
2020-12-01 13:01:53 -0800

    2 working better than 1 now

--- 2/sum-mult.py
+++ 2/sum-mult.py
@@ -5,6 +5,7 @@ with open("input.txt") as file:
             ints.append(int(line))
 
 for i_1, v_1 in enumerate(ints):
-    for i_2, v_2 in enumerate(ints[(i_1 + 1):]):
-        if v_1 + v_2 == 2020:
-            print(f"[{i_1}, {i_2}]: {v_1} * {v_2} = {v_1 * v_2}")
+    for i_2, v_2 in enumerate(ints[(i_1 + 1):], start=(i_1 + 1)):
+        for i_3, v_3 in enumerate(ints[(i_2 + 1):], start=(i_2 + 1)):
+            if v_1 + v_2 + v_3 == 2020:
+                print(f"[{i_1}, {i_2}, {i_3}]: {v_1} * {v_2} * {v_3} = {v_1 * v_2 * v_3}")
