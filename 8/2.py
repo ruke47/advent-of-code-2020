afa59a736886cfba38e92733c1d01f646f86bfdf
Andrew Rueckert <ruke47@users.noreply.github.com>
2020-12-09 14:38:53 -0800

    8-2 final

--- 8/2.py
+++ 8/2.py
@@ -1,17 +1,31 @@
 
 class StateMachine:
     # instructions= cmd, value
-    def __init__(self, instructions, pc=0, acc=0, seen_pc=set(), verbose=False):
-        self.instructions = instructions
-        self.pc = pc
-        self.acc = acc
-        self.verbose = verbose
-        self.seen_pc = seen_pc
+    def __init__(self, instructions=None, base_state_machine=None, verbose=False):
+        if instructions:
+            self.name = "Base"
+            self.instructions = instructions
+            self.pc = 0
+            self.acc = 0
+            self.verbose = verbose
+            self.seen_pc = set()
+        elif base_state_machine: 
+            self.name = f"Clone({base_state_machine.pc})"
+            self.instructions = base_state_machine.instructions
+            self.pc = base_state_machine.pc
+            self.acc = base_state_machine.acc
+            self.verbose = base_state_machine.verbose
+            self.seen_pc = base_state_machine.seen_pc.copy()
+        else:
+            raise RuntimeError("Must initialize with instructions or base state machine")
+
+    def current_instruction(self):
+        return self.instructions[self.pc]
 
     def step(self):
         if self.pc >= len(self.instructions):
             raise IndexError("PC outside of instruction range")
-        cmd, value = self.instructions[self.pc]
+        cmd, value = self.current_instruction()
         if cmd == "acc":
             self.acc += value
             self.pc += 1
@@ -27,23 +41,25 @@ class StateMachine:
             self.seen_pc.add(self.pc)
             if self.pc >= len(self.instructions):
                 return True
-            if branch and self.instructions[self.pc][0] == "jmp":
-                clone = StateMachine(self.instructions, pc=self.pc + 1, acc=self.acc, seen_pc=self.seen_pc)
-                if (clone.run_till_done()):
+            cmd, val = self.current_instruction()
+            if branch and cmd in {"jmp", "nop"}:
+                clone = StateMachine(base_state_machine=self)
+                if cmd == "jmp":
+                    clone.pc += 1
+                elif cmd == "nop":
+                    clone.pc += val
+                if clone.run_till_done(branch=False):
                     print(f"Clone ACC: {clone.acc}")
                     return True
-            elif branch and self.instructions[self.pc][1] == "nop":
-                clone = StateMachine(self.instructions, pc=self.pc + self.instructions[self.pc][1], acc=self.acc, seen_pc=self.seen_pc)
-                if (clone.run_till_done()):
-                    print(f"Clone ACC: {clone.acc}")
-                    return True
-            self.step()
             self.say_state()
+            self.step()
+        if branch:
+            print("Never found an out.")
         return False 
 
     def say_state(self):
         if self.verbose:
-            print(f"PC: {self.pc}\nACC: {self.acc}\nCMD: {self.instructions[self.pc]}\n")
+            print(f"Name: {self.name}\n  PC: {self.pc}\n  ACC: {self.acc}\n  CMD: {self.instructions[self.pc]}\n")
 
 if __name__ == "__main__":
     instructions = []
@@ -52,7 +68,7 @@ if __name__ == "__main__":
             parts = line.split()
             instructions.append((parts[0], int(parts[1])))
 
-    sm = StateMachine(instructions, True)
+    sm = StateMachine(instructions=instructions, verbose=True)
     final_acc = sm.run_till_done(branch=True)
 
 
