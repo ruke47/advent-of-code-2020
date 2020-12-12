e3aa79a25cbfac74ef10c27faa95c286f3ad2e26
Andrew Rueckert <ruke47@users.noreply.github.com>
2020-12-11 17:06:38 -0800

    10-1 golf

--- 10/1.py
+++ 10/1.py
@@ -1,23 +1,15 @@
-
 adapters = []
 with open("input.txt") as file:
     for line in file:
         adapters.append(int(line))
 
 adapters.sort()
+adapters.append(adapters[-1] + 3)
 
 prior = 0
-ones = 0
-threes = 0
-for adapter in adapters:
-    if adapter == prior:
-        print(f"Saw {adapter} twice in a row")
-    elif adapter == prior + 1:
-        ones += 1
-    elif adapter == prior + 3:
-        threes += 1
-    elif adapter > prior + 3:
-        print(f"Saw jump > 3 between {prior} and {adapter}.")
-    prior = adapter
-threes += 1
-print(f"Ones: {ones} Threes: {threes} Mult: {ones * threes}")
+diffs = {}
+for val in adapters:
+    diffs[val - prior] = diffs.get(val-prior, 0) + 1
+    prior = val
+print(diffs)
+print(f"Mult: {diffs[1] * diffs[3]}")
