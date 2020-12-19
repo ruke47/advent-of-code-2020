ef8885acc7bebd30773997d422418be2e8dc27eb
Andrew Rueckert <ruke47@users.noreply.github.com>
2020-12-18 23:28:44 -0800

    Comment 19-2

--- 19/2.py
+++ 19/2.py
@@ -8,16 +8,20 @@ class Pattern:
         self.rule_text = rule_text
 
     def detangle(self):
+        # if the rule is quoted, just take the literal character
         if self.rule_text.startswith('"'):
             return self.rule_text[1]
 
         regex_str = ""
         for part in self.rule_text.split(" "):
+            # if the part refers to another rule, append that rule's text 
             if part.isdecimal():
                 regex_str += self.rule_map[part].detangle()
+            # if the part is non-numeric, treat it as a string literal | or + or, uh, (?R)?
             else: 
                 regex_str += part
 
+        # surround yourself with a non-capturing group so letters don't smush together
         return "(?:" + regex_str + ")"
 
 def main():
@@ -30,7 +34,9 @@ def main():
             else:
                 break
 
+        # `8: 42 | 42 8` means "at least 1 of whatever 42 is
         rule_map["8"] = Pattern(rule_map, "42 +")
+        # `11: 42 31 | 42 11 31` means "at least one 42, followed by the same number of 31's"
         rule_map["11"] = Pattern(rule_map, "42 (?R)? 31")
 
         gross_regex_str = rule_map["0"].detangle()
